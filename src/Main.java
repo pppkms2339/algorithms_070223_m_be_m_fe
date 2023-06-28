404f0bd5ce8ecf5c4b089bd4930fb87b8cd17fc1
Anton Loshmanov <pppkms2339@gmail.com>
2023-06-28 21:55:10 +1000

    lesson5_28_06_2023

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,67 @@
 public class Main {
     public static void main(String[] args) {
-        move(2, 1, 2, 3);
+        int[] arr1 = {1, 3, 5, 9};
+        int[] arr2 = {4, 7};
+        System.out.println(findNumber(arr1, arr2, 3));
+        System.out.println(binarySearchRec(arr1, 11));
     }
 
-    private static void move(int n, int source, int destination, int helper) {
-        if (n == 0) {
-            return;
+    private static int findNumber(int[] arr1, int[] arr2, int k) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        if (k < 0 || k >= len1 + len2) {
+            System.out.println("Error");
+            return -1;
         }
-        move(n - 1, source, helper, destination);
-        System.out.println(source + "->" + destination);
-        move(n - 1, helper, destination, source);
+        int i = 0, j = 0;
+        while (i < len1 && j < len2) {
+            if (arr1[i] < arr2[j]) {
+                if (i + j == k) {
+                    return arr1[i];
+                } else {
+                    i++;
+                }
+            } else {
+                if (i + j == k) {
+                    return arr2[j];
+                } else {
+                    j++;
+                }
+            }
+        }
+        while (i < len1) {
+            if (i + j == k) {
+                return arr1[i];
+            } else {
+                i++;
+            }
+        }
+        while (j < len2) {
+            if (i + j == k) {
+                return arr2[j];
+            } else {
+                j++;
+            }
+        }
+        return -1;
     }
 
-    private static int fact(int n) {
-        if (n == 1) {
-            return 1;
+    public static int binarySearchRec(int[] arr, int target) {
+        return binarySearchRec(arr, target, 0, arr.length - 1);
+    }
+
+    private static int binarySearchRec(int[] arr, int target, int left, int right) {
+        // Базовый случай (число не найдено)
+        if (left > right) {
+            return -1;
         }
-        return fact(n - 1) * n;
+        int mid = (left + right) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            return binarySearchRec(arr, target, mid + 1, right);
+        } else {
+            return binarySearchRec(arr, target, left, mid - 1);
+        }
     }
 }
